04c36d51f5377bf198b5212dc6c6c9158413454e
MichalCiesliczka <michal.ciesliczka@xsolve.pl>
2017-11-11 16:06:25 +0100

    Adjust to 01-simple-form

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,6 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import { reducer as formReducer } from 'redux-form'
-
-import app from './appReducer';
 
 export default combineReducers({
-  app,
   routing: routerReducer,
-  form: formReducer,
 })
